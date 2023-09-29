2ddf3b14f1569a37b6dd910cbc6013fe571e0628
greg0788 <g.girault@codeur.online>
2023-09-29 09:04:59 +0200

    migration

---
+++ migrations/Version20230928142523.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230928142523 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE `like` (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, post_id INT NOT NULL, INDEX IDX_AC6340B3A76ED395 (user_id), INDEX IDX_AC6340B34B89032C (post_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE `like` ADD CONSTRAINT FK_AC6340B3A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE `like` ADD CONSTRAINT FK_AC6340B34B89032C FOREIGN KEY (post_id) REFERENCES post (id)');
+        $this->addSql('ALTER TABLE comment ADD user_id INT NOT NULL, ADD post_id INT NOT NULL');
+        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526CA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526C4B89032C FOREIGN KEY (post_id) REFERENCES post (id)');
+        $this->addSql('CREATE INDEX IDX_9474526CA76ED395 ON comment (user_id)');
+        $this->addSql('CREATE INDEX IDX_9474526C4B89032C ON comment (post_id)');
+        $this->addSql('ALTER TABLE news ADD user_id INT NOT NULL');
+        $this->addSql('ALTER TABLE news ADD CONSTRAINT FK_1DD39950A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_1DD39950A76ED395 ON news (user_id)');
+        $this->addSql('ALTER TABLE notification ADD user_id INT NOT NULL');
+        $this->addSql('ALTER TABLE notification ADD CONSTRAINT FK_BF5476CAA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_BF5476CAA76ED395 ON notification (user_id)');
+        $this->addSql('ALTER TABLE post ADD user_id INT NOT NULL, ADD category_id INT NOT NULL');
+        $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8DA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8D12469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+        $this->addSql('CREATE INDEX IDX_5A8A6C8DA76ED395 ON post (user_id)');
+        $this->addSql('CREATE INDEX IDX_5A8A6C8D12469DE2 ON post (category_id)');
+        $this->addSql('ALTER TABLE user ADD chocolate_shop_id INT NOT NULL');
+        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D6496563BFA9 FOREIGN KEY (chocolate_shop_id) REFERENCES chocolate_shop (id)');
+        $this->addSql('CREATE INDEX IDX_8D93D6496563BFA9 ON user (chocolate_shop_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE `like` DROP FOREIGN KEY FK_AC6340B3A76ED395');
+        $this->addSql('ALTER TABLE `like` DROP FOREIGN KEY FK_AC6340B34B89032C');
+        $this->addSql('DROP TABLE `like`');
+        $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526CA76ED395');
+        $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526C4B89032C');
+        $this->addSql('DROP INDEX IDX_9474526CA76ED395 ON comment');
+        $this->addSql('DROP INDEX IDX_9474526C4B89032C ON comment');
+        $this->addSql('ALTER TABLE comment DROP user_id, DROP post_id');
+        $this->addSql('ALTER TABLE news DROP FOREIGN KEY FK_1DD39950A76ED395');
+        $this->addSql('DROP INDEX IDX_1DD39950A76ED395 ON news');
+        $this->addSql('ALTER TABLE news DROP user_id');
+        $this->addSql('ALTER TABLE notification DROP FOREIGN KEY FK_BF5476CAA76ED395');
+        $this->addSql('DROP INDEX IDX_BF5476CAA76ED395 ON notification');
+        $this->addSql('ALTER TABLE notification DROP user_id');
+        $this->addSql('ALTER TABLE post DROP FOREIGN KEY FK_5A8A6C8DA76ED395');
+        $this->addSql('ALTER TABLE post DROP FOREIGN KEY FK_5A8A6C8D12469DE2');
+        $this->addSql('DROP INDEX IDX_5A8A6C8DA76ED395 ON post');
+        $this->addSql('DROP INDEX IDX_5A8A6C8D12469DE2 ON post');
+        $this->addSql('ALTER TABLE post DROP user_id, DROP category_id');
+        $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D6496563BFA9');
+        $this->addSql('DROP INDEX IDX_8D93D6496563BFA9 ON user');
+        $this->addSql('ALTER TABLE user DROP chocolate_shop_id');
+    }
+}
