872272b8ad4afca5e8d495022c53935bcaa6a78c
ahmed58000 <ahmed_k@laposte.net>
2023-10-03 14:14:28 +0200

    FormulaireInscriptionAdminEmployés

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -10,7 +10,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RegistrationController extends AbstractController
 {
@@ -21,10 +20,10 @@ class RegistrationController extends AbstractController
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
-
         if ($form->isSubmitted() && $form->isValid()) {
             // encode the plain password
-
+            $user->setRoles(['ROLE_SUPER_ADMIN']);
+            
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
                     $user,
@@ -43,4 +42,66 @@ class RegistrationController extends AbstractController
             'registrationForm' => $form->createView(),
         ]);
     }
+
+    #[Route('/register/admin', name: 'app_register_admin')]
+    public function registerAdmin(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+          
+            $user->setRoles(['ROLE_ADMIN']);
+
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+           
+
+            return $this->redirectToRoute('app_preview_error'); 
+        }
+
+        return $this->render('registration/admin_register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/register/employe', name: 'app_register_employe')]
+    public function registerEmployee(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $user->setRoles(['ROLE_EMPLOYE']);
+
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+           
+
+            return $this->redirectToRoute('app_preview_error'); 
+        }
+
+        return $this->render('registration/employe_register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
 }
+
+
