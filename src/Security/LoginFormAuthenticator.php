5c4bccf8d07acbac09d3b38e481cc6523a30b71c
greg0788 <g.girault@codeur.online>
2023-10-13 12:59:45 +0200

    profil_style.less

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -51,6 +51,8 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         // For example:
         // return new RedirectResponse($this->urlGenerator->generate('some_route'));
         return new RedirectResponse($this->urlGenerator->generate('profile'));
+        
+        // faire condition de redirection selon les droits (admin, user)
     }
 
     protected function getLoginUrl(Request $request): string
