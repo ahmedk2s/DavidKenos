6df208323ce3ceb75b1fda361c62551ca6d5accb
greg0788 <g.girault@codeur.online>
2023-10-23 15:42:57 +0200

    responsive tablet accueil admin

--- public/assets/styles/javascrypt/admin.js
+++ public/assets/styles/javascrypt/admin.js
@@ -1,15 +1,9 @@
-// Cette fonction s'assure que le menu est caché au chargement de la page
-window.onload = () => {
+document.addEventListener("DOMContentLoaded", () => {
   const navContainer = document.getElementById("nav-container");
-  navContainer.style.display = "none";
-};
 
-document.addEventListener("DOMContentLoaded", (event) => {
   document
     .getElementById("burger-menu-button")
     .addEventListener("click", () => {
-      const navContainer = document.getElementById("nav-container");
-
       if (navContainer.style.display === "block") {
         navContainer.style.display = "none";
       } else {
@@ -17,4 +11,3 @@ document.addEventListener("DOMContentLoaded", (event) => {
       }
     });
 });
-
