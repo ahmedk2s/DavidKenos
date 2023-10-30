4ff6bbe42280e193211482ff64a0bffecb92db71
greg0788 <g.girault@codeur.online>
2023-10-30 10:34:33 +0100

    responsive edit users

---
+++ src/EventListener/UserListener.php
@@ -0,0 +1,24 @@
+<?php
+// src/EventListener/UserListener.php
+namespace App\EventListener;
+
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Symfony\Component\Security\Core\Security;
+use Twig\Environment;
+
+class UserListener
+{
+    private $twig;
+    private $security;
+
+    public function __construct(Environment $twig, Security $security)
+    {
+        $this->twig = $twig;
+        $this->security = $security;
+    }
+
+    public function onKernelController(ControllerEvent $event): void
+    {
+        $this->twig->addGlobal('user', $this->security->getUser());
+    }
+}
