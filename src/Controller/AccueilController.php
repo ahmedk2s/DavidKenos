f87c99440bbcc11b626b3fba7e273b6fade1fe64
ahmed58000 <ahmed_k@laposte.net>
2023-11-10 10:44:22 +0100

    ajoutimage&récupération

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -6,18 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\NewsRepository;
+use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Post; // Assurez-vous d'importer l'entité Post ici
 
 class AccueilController extends AbstractController
 {
     #[Route('/accueil', name: 'app_accueil')]
-    public function index(NewsRepository $newsRepository): Response
+    public function index(NewsRepository $newsRepository, EntityManagerInterface $entityManager): Response
     {
         $user = $this->getUser();
+
+        // Récupérez les six derniers posts de la base de données, triés par ordre décroissant de la date de création
+        $postRepository = $entityManager->getRepository(Post::class); // Utilisez l'entité Post ici
+        $posts = $postRepository->findBy([], ['date_creation' => 'DESC'], 6);
+
         $news = $newsRepository->findAll();
 
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+            'posts' => $posts,
             'news' => $news,
         ]);
     }
 }
+
--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -8,14 +8,27 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: PostRepository::class)]
+#[Vich\Uploadable]
 class Post
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    #[Assert\NotBlank(message: "Le titre ne peut pas être vide")]
+    #[Assert\Length(
+        min: 2,
+        max: 100,
+        minMessage: "Le titre doit contenir au moins {{ limit }} caractères",
+        maxMessage: "Le titre ne peut pas contenir plus de {{ limit }} caractères"
+    )]
+    private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT)]
     #[Assert\NotBlank(message: "Le texte ne peut pas être vide")]
@@ -26,6 +39,12 @@ class Post
         maxMessage: "Le texte ne peut pas contenir plus de {{ limit }} caractères"
     )]
     private ?string $text = null;
+
+    #[Vich\UploadableField(mapping: 'products', fileNameProperty: 'imageName')]
+    private ?File $imageFile = null;
+
+    #[ORM\Column(type: 'string', nullable: true)]
+    private ?string $imageName = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     private ?\DateTimeInterface $date_creation = null;
@@ -47,16 +66,6 @@ class Post
     #[ORM\OneToMany(mappedBy: 'post', targetEntity: Comment::class, orphanRemoval: true)]
     private Collection $comments;
 
-    #[ORM\Column(length: 100)]
-    #[Assert\NotBlank(message: "Le titre ne peut pas être vide")]
-    #[Assert\Length(
-        min: 2,
-        max: 100,
-        minMessage: "Le titre doit contenir au moins {{ limit }} caractères",
-        maxMessage: "Le titre ne peut pas contenir plus de {{ limit }} caractères"
-    )]
-    private ?string $title = null;
-
     #[ORM\Column(length: 255, unique: true)]
     private ?string $slug = null;
 
@@ -72,6 +81,17 @@ class Post
         return $this->id;
     }
 
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+        return $this;
+    }
+
     public function getText(): ?string
     {
         return $this->text;
@@ -81,6 +101,41 @@ class Post
     {
         $this->text = $text;
         return $this;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
     }
 
     public function getDateCreation(): ?\DateTimeInterface
@@ -199,17 +254,6 @@ class Post
             }
         }
 
-        return $this;
-    }
-
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
-
-    public function setTitle(string $title): static
-    {
-        $this->title = $title;
         return $this;
     }
 
